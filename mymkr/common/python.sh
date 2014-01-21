58d5fe91f4679c9d48bc0e92004bccd3e56e9020
Xendarboh Sensorii <xen@shastafareye.net>
2014-01-21 12:26:32 -0800

    updated common/python.sh to use "die" function
    more functions in mymkr

--- mymkr/common/python.sh
+++ mymkr/common/python.sh
@@ -1,7 +1,7 @@
 # $1: location in /src/ directory (usually package name)
 # $2...: all other arguments get appended to the "setup.py install" command
 common_python() {
-	cd $MYMKR_PREFIX/src/$1 || exit 1
-	python ./setup.py build || exit 1
-	python ./setup.py install --prefix $MYMKR_PREFIX ${@:2} || exit 1
+	cd $MYMKR_PREFIX/src/$1 || die 'common_python: could not access source directory'
+	python ./setup.py build || die 'common_python: build failed'
+	python ./setup.py install --prefix $MYMKR_PREFIX ${@:2} || die 'common_python: install failed'
 }
