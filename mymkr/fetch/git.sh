9ef752d7c7d3a0df44c908b878ee71077943b438
Xendarboh Sensorii <xen@shastafareye.net>
2014-01-26 21:49:40 -0800

    MOD: git uses mymkr messages on failure

--- mymkr/fetch/git.sh
+++ mymkr/fetch/git.sh
@@ -5,11 +5,11 @@ gitsrc() {
 	if [ ! -d $MYMKR_PREFIX/src/$1 ]
 	then
 		msg_info "$1 source dir does not exist, git clone'ing..."
-		git clone $2 $MYMKR_PREFIX/src/$1
+		git clone $2 $MYMKR_PREFIX/src/$1 || die "git clone failed"
 	else
 		msg_info "$1 source dir exists, git pull'ing..."
 		cd $MYMKR_PREFIX/src/$1
-		git pull
+		git pull || die "git pull failed"
 		cd -
 	fi
 
