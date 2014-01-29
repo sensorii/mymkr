c753cfb15d71f197c8be37dd57a3da6ec654297b
Xendarboh Sensorii <xen@shastafareye.net>
2014-01-28 16:29:10 -0800

    MOD: pkgs/vanitygen install executables in bin/

--- mymkr/pkgs/vanitygen.sh
+++ mymkr/pkgs/vanitygen.sh
@@ -1,5 +1,7 @@
 gitsrc $1 https://github.com/samr7/vanitygen.git
-cd $MYMKR_PREFIX/src/$1
+cd $MYMKR_PREFIX/src/$1 || die "source dir not found"
 export CFLAGS="-I$MYMKR_PREFIX/include -L$MYMKR_PREFIX/lib"
-make clean
-make -e
+make clean || die "make clean failed"
+make -e || die "make failed"
+cp -a vanitygen $MYMKR_PREFIX/bin || die "vanitygen executable not accessible"
+cp -a keyconv $MYMKR_PREFIX/bin/vanitygen-keyconv || die "keyconv executable not accessible"
