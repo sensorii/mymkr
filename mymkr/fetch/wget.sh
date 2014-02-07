30e6be74c147cb72f90dafd13396075f412485c2
Xendarboh Sensorii <xen@shastafareye.net>
2014-02-07 00:37:30 -0800

    MOD: fetch/wget: extract based on detected extension

--- mymkr/fetch/wget.sh
+++ mymkr/fetch/wget.sh
@@ -3,11 +3,10 @@
 # $3: archive type - detected by default, argument enforces archive type to extract
 fetch_wget() {
 	URL=$2
-	test -z "$WGET" && _WGET='wget'
 
 	if [ -d $MYMKR_PREFIX/src/$1 ]
 	then
-		msg_warning "source exists, considering fetched"
+		msg_warning 'source exists, considering fetched'
 	else
 		cd $MYMKR_PREFIX/src
 		PKG=$(basename $URL)
@@ -16,32 +15,35 @@ fetch_wget() {
 			$WGET "$URL" || die "wget failed: $URL"
 		fi
 
-		if [ -z "$3" ]
+		if [ $_EXTRACT_ARCHIVE -eq 0 ]
 		then
-			# Reference: http://stackoverflow.com/a/965072
-			#filename=$(basename "$URL")
-			extension="${PKG##*.}"
-			#A="${filename%.*}"
+			msg_warning "skipping extract"
 		else
-			extension="$3"
+			if [ -z "$3" ]
+			then
+				# Reference: http://stackoverflow.com/a/965072
+				extension="${PKG##*.}"
+			else
+				extension="$3"
+			fi
+
+			case ${extension} in
+				tgz|tar.gz)
+					tar -xzf $PKG || die "xtract archive failed"
+					;;
+
+				tbz2|tar.bz2)
+					tar -xjf $PKG || die "xtract archive failed"
+					;;
+
+				zip)
+					unzip $PKG || die "unzip failed"
+					;;
+
+				*)
+					die "unknown archive type: $extension"
+					;;
+			esac
 		fi
-
-		case ${extension} in
-			tgz|tar.gz)
-				tar -xzf $PKG || die "xtract archive failed"
-				;;
-
-			tbz2|tar.bz2)
-				tar -xjf $PKG || die "xtract archive failed"
-				;;
-
-			zip)
-				unzip $PKG || die "unzip failed"
-				;;
-
-			*)
-				die "unknown archive type: $A"
-				;;
-		esac
 	fi
 }
