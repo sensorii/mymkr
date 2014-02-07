290c9435faf8c77359044acdf9104dabc98b1478
Xendarboh Sensorii <xen@shastafareye.net>
2014-02-07 05:09:26 -0800

    NEW: common/autotools: _MAKE_ENV_OVERRIDE to do "make -e", added more path stuff
    exports of LDFLAGS AND CPPFLAGS were added before the configure step for
    some packages, such as cudaminer, that pull configuration from
    environment variables.

--- mymkr/common/autotools.sh
+++ mymkr/common/autotools.sh
@@ -1,6 +1,10 @@
 # $1: location in /src/ directory (usually package name)
 # $2...: all other arguments get appended to the ./configure command
 common_autotools () {
+	test -z "${_MAKE}" && _MAKE=1
+	test -z "${_MAKE_INSTALL}" && _MAKE_INSTALL=1
+	test -z "${_MAKE_ENV_OVERRIDE}" && _MAKE_ENV_OVERRIDE=0
+
  	cd $MYMKR_PREFIX/src/$1 || die "source dir does not exist?: $MYMKR_PREFIX/src/$1"
  	#make clean
 
@@ -16,14 +20,22 @@ common_autotools () {
 
  	if [ -x './configure' ]
  	then
+		export LDFLAGS="-L${MYMKR_PREFIX}/lib ${LDFLAGS}"
+		export CPPFLAGS="-I$MYMKR_PREFIX/include ${CPPFLAGS}"
  		./configure --prefix=$MYMKR_PREFIX "${@:2}" || die 'configure failed'
+	fi
+
+	OPTS=
+	if [ $_MAKE_ENV_OVERRIDE -eq 1 ]
+	then
+		OPTS='-e'
 	fi
 
  	if [ $_MAKE -eq 0 ]
 	then
 		msg_warning "skipping make"
 	else
-		make $JOBS || die 'make failed'
+		make $OPTS $JOBS || die 'make failed'
 	fi
 
  	if [ $_MAKE_INSTALL -eq 0 ]
@@ -31,7 +43,7 @@ common_autotools () {
 		msg_warning "skipping make install"
 	else
  		# PREFIX given again for packages without a configure script (like jack_oscrolloscope)
-		make PREFIX=$MYMKR_PREFIX install || die 'make install failed'
+		make $OPTS PREFIX=$MYMKR_PREFIX install || die 'make install failed'
  	fi
 
 	return 0
