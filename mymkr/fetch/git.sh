29a3490a03102f9fb0997f5967c1d041a06cc52c
Xendarboh Sensorii <xen@shastafareye.net>
2014-02-07 00:35:18 -0800

    NEW: fetch/git: _GIT_RESET
    added option to reset git HEAD, usefull when you want to force overwrite
    of any local changes to source

--- mymkr/fetch/git.sh
+++ mymkr/fetch/git.sh
@@ -2,6 +2,8 @@
 # $2: git url
 # $3: (optional) git tag or branch to checkout
 gitsrc() {
+	test -z "${_GIT_RESET}" && _GIT_RESET=0
+
 	if [ ! -d $MYMKR_PREFIX/src/$1 ]
 	then
 		msg_info "$1 source dir does not exist, git clone'ing..."
@@ -9,14 +11,21 @@ gitsrc() {
 	else
 		msg_info "$1 source dir exists, git pull'ing..."
 		cd $MYMKR_PREFIX/src/$1
-		git pull || die "git pull failed"
+
+		if [ $_GIT_RESET -eq 1 ]
+		then
+			# http://stackoverflow.com/a/4550966
+			msg_warning 'performing git reset --hard HEAD'
+			git reset --hard HEAD
+		fi
+		git pull || die 'git pull failed'
 		cd -
 	fi
 
 	if [ ! -z "$3" ]
 	then
 		cd $MYMKR_PREFIX/src/$1
-		git checkout $3 || die "git checkout $3"
+		git checkout $3 || die "git checkout ${3}"
 		cd -
 	fi
 }
