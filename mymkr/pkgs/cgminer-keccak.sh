be53c1c6327c7f1b48339bf1916a0b1bdb054bbf
X <xen@shastafareye.net>
2014-02-20 10:32:58 +0100

    MOD: pkgs/cgminer-keccak comples on BAMT/SMOS

--- mymkr/pkgs/cgminer-keccak.sh
+++ mymkr/pkgs/cgminer-keccak.sh
@@ -2,5 +2,6 @@ _GIT_RESET=1
 gitsrc $1 'https://github.com/reorder/cgminer_keccak.git'
 _MAKE_ENV_OVERRIDE=1
 export NOCONFIGURE='1'
-export CFLAGS='-O3 -Wall -march=native'
+export CFLAGS='-O3 -Wall -march=native -I /opt/AMDAPP/include/'
+export LDFLAGS="-L/opt/AMDAPP/lib/x86"
 common_autotools $1 --enable-keccak --enable-opencl
