efd0985ef32b6f5e08cc3fd6fc64814343ca7a27
Xendarboh Sensorii <xen@shastafareye.net>
2014-03-04 13:15:52 -0800

    NEW: common_coind: _COMMON_COIND_BIN_NAME

--- mymkr/common/coind.sh
+++ mymkr/common/coind.sh
@@ -2,6 +2,7 @@
 common_coind ()
 {
 	test -z "${_COMMON_COIND_LEVELDB_FIX}" && _COMMON_COIND_LEVELDB_FIX=0
+	test -z "${_COMMON_COIND_BIN_NAME}" && _COMMON_COIND_BIN_NAME="${1}d"
 
 	cd $MYMKR_PREFIX/src/$1/src || die 'cd source dir failed'
 
@@ -16,5 +17,6 @@ common_coind ()
 	export CPPFLAGS="-I$MYMKR_PREFIX/include"
 	export LDFLAGS="-L${MYMKR_PREFIX}/lib"
 	make $JOBS -e PREFIX=$MYMKR_PREFIX USE_UPNP=- -f makefile.unix || die 'make failed'
-	cp -a "${1}d" $MYMKR_PREFIX/bin || die "could not install ${1}d"
+	cp -a "${_COMMON_COIND_BIN_NAME}" $MYMKR_PREFIX/bin \
+	 || die "could not install ${_COMMON_COIND_BIN_NAME}"
 }
