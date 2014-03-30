9473ca98d9c82991ee142ba9be6942e8eb24203a
Xendarboh Sensorii <xen@shastafareye.net>
2014-03-30 13:24:01 -0700

    [mod] updated gitsrc of h2ocoin, new release

--- mymkr/pkgs/h2ocoin.sh
+++ mymkr/pkgs/h2ocoin.sh
@@ -1,8 +1,7 @@
-gitsrc $1 https://github.com/CoinDev-io/H2O
+gitsrc $1 https://github.com/h2o-coin/H2Ocoin.git
 
 mkdir $MYMKR_PREFIX/src/$1/src/obj
 chmod u+x $MYMKR_PREFIX/src/$1/src/leveldb/build_detect_platform
 
-_COMMON_COIND_BIN_NAME=H2Od
 _COMMON_COIND_LEVELDB_FIX=1
 common_coind $1
