21f8d603cbfd89395bb529366ced71ea50bc091e
Xen <xen@smarthash.io>
2014-05-14 10:04:20 +0000

    [mod] added CXX/CPPFLAGS to beagleboneblack profile

--- mymkr/profiles/beagleboneblack.sh
+++ mymkr/profiles/beagleboneblack.sh
@@ -1,1 +1,3 @@
 export CFLAGS='-O2 -Wall -march=armv7-a -mtune=cortex-a8 -mfloat-abi=hard -mfpu=neon'
+export CXXFLAGS='-O2 -Wall -march=armv7-a -mtune=cortex-a8 -mfloat-abi=hard -mfpu=neon'
+export CPPFLAGS='-O2 -Wall -march=armv7-a -mtune=cortex-a8 -mfloat-abi=hard -mfpu=neon'
