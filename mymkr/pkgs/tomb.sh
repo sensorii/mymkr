42263259c2b526e5855dcb9de5535938b161ee14
xen <xen@smarthash.io>
2014-08-10 00:56:08 -0700

    [fix] install tomb man page in correct directory

--- mymkr/pkgs/tomb.sh
+++ mymkr/pkgs/tomb.sh
@@ -3,5 +3,6 @@ gitsrc $1 https://github.com/dyne/Tomb.git
 common_autotools $1 || die "error"
 cd $MYMKR_PREFIX/src/$1
 cp -a tomb $MYMKR_PREFIX/bin/tomb
-test ! -d $MYMKR_PREFIX/share/man && mkdir -p $MYMKR_PREFIX/share/man
-cp -a doc/tomb.1 $MYMKR_PREFIX/share/man/tomb.1
+M=$MYMKR_PREFIX/share/man/man1
+test ! -d $M && mkdir -p $M
+cat doc/tomb.1 | gzip > $M/tomb.1.gz
