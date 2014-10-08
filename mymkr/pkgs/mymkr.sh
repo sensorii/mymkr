aaacfb6fe725cf50a32ff1c0f0a704daad9ad09b
Xendarboh Sensorii <xen@shastafareye.net>
2014-10-08 04:03:56 +0000

    pkgs/mymkr: do not rm current profile if no profile is given as 2nd arg

--- mymkr/pkgs/mymkr.sh
+++ mymkr/pkgs/mymkr.sh
@@ -1,6 +1,6 @@
 # @param $2: set profile in mymkr/profiles
 
-# knstall mymkr in bin directory for easy usage
+# install mymkr in bin directory for easy usage
 ln -sf "${MYMKR_PREFIX}/mymkr/mymkr" "${MYMKR_PREFIX}/bin/mymkr"
 
 # set profile
@@ -9,6 +9,4 @@ then
 	cd "${MYMKR_PREFIX}/mymkr/profiles"
 	test -f "${2}.sh" || die "profile not found: ${2}"
 	ln -sf "${2}.sh" ./current.sh
-else
-	rm -f "${MYMKR_PREFIX}/mymkr/profiles/current.sh"
 fi
